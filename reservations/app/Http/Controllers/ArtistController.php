d4a395759677278223ea4006981529a36bede7bb
albarry00 <albarry00@gmail.com>
2024-02-15 13:21:46 +0100

    form login + regiser

--- reservations/app/Http/Controllers/ArtistController.php
+++ reservations/app/Http/Controllers/ArtistController.php
@@ -124,3 +124,5 @@ class ArtistController extends Controller
     }
 
 }
+
+
--- reservations/routes/web.php
+++ reservations/routes/web.php
@@ -8,9 +8,23 @@ use App\Http\Controllers\ArtistController;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-//…
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
 
 Route::get('/', function () {
     return view('welcome');
@@ -30,6 +44,3 @@ Route::post('/artist', [ArtistController::class, 'store'])->name('artist.store')
 
 Route::delete('/artist/{id}', [ArtistController::class, 'destroy'])
 	->where('id', '[0-9]+')->name('artist.delete');
-
-
-       
